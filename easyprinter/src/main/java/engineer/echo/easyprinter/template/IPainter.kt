2450c2c25420cf41645874fc44a3c6484b234c9e
plucky <py@meitu.com>
2019-06-25 23:37:47 +0800

    [feature] print view test

--- easyprinter/src/main/java/engineer/echo/easyprinter/template/IPainter.kt
+++ easyprinter/src/main/java/engineer/echo/easyprinter/template/IPainter.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
 import androidx.core.graphics.applyCanvas
+import engineer.echo.easylib.clipTransparentEdge
 import engineer.echo.easyprinter.EasyPrinter
 import engineer.echo.easyprinter.command.CommandBox
 import engineer.echo.easyprinter.command.CommandBox.PRINT
@@ -27,20 +28,23 @@ abstract class IPainter {
 
     abstract fun draw(canvas: Canvas)
 
-    fun toBitmap(): Bitmap {
-        return Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888).applyCanvas(::draw)
+    fun toBitmap(clipTransParent: Boolean = true): Bitmap {
+        Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888).applyCanvas(::draw).also {
+            return if (clipTransParent) it.clipTransparentEdge() else it
+        }
     }
 
     fun print(
         device: BluetoothDevice,
         align: Paint.Align = Paint.Align.CENTER,
-        maxWidth: Int = 360
+        maxWidth: Int = 360,
+        clipTransParent: Boolean = true
     ) {
         val data = when (align) {
             Paint.Align.LEFT -> CommandBox.ALIGN_LEFT
             Paint.Align.RIGHT -> CommandBox.ALIGN_RIGHT
             else -> CommandBox.ALIGN_CENTER
-        }.plus(toBitmap().scaleToFit(maxWidth).toPrintByte(maxWidth)).plus(PRINT)
+        }.plus(toBitmap(clipTransParent).scaleToFit(maxWidth).toPrintByte(maxWidth)).plus(PRINT)
         EasyPrinter.get().startPrintTask(device, data)
     }
 }
