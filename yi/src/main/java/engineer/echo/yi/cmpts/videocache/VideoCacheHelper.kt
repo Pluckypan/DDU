fea92781b23ca3d421e0676e6ab1ee51b5f1cb77
plucky <py@meitu.com>
2020-02-03 15:39:58 +0800

    [feature] Yi: Add VideoCacheProxy

---
+++ yi/src/main/java/engineer/echo/yi/cmpts/videocache/VideoCacheHelper.kt
@@ -0,0 +1,20 @@
+package engineer.echo.yi.cmpts.videocache
+
+import com.danikula.videocache.HttpProxyCacheServer
+import engineer.echo.yi.YiApp
+import retrofit2.http.Url
+
+object VideoCacheHelper {
+
+    private val proxy by lazy {
+        HttpProxyCacheServer.Builder(YiApp.getApp())
+            .maxCacheSize(1024 * 1024 * 1024) // 1GB
+            .maxCacheFilesCount(20)
+            .build()
+    }
+
+    fun getProxyUrl(@Url originUrl: String, allowCachedFileUri: Boolean = true): String =
+        proxy.getProxyUrl(originUrl, allowCachedFileUri)
+
+    fun isCached(@Url originUrl: String): Boolean = proxy.isCached(originUrl)
+}
