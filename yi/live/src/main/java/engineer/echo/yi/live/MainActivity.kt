7063b9625e0a871268ceef1434f821ab6d93bf54
plucky <py@meitu.com>
2020-02-07 13:54:49 +0800

    [feature] Yi: add modules: live & im & common & producer & consumer

---
+++ yi/live/src/main/java/engineer/echo/yi/live/MainActivity.kt
@@ -0,0 +1,12 @@
+package engineer.echo.yi.live
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- yi/src/androidTest/java/engineer/echo/yi/ExampleInstrumentedTest.kt
+++ yi/src/androidTest/java/engineer/echo/yi/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package engineer.echo.yi
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
