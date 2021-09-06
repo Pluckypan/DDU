120e8f25747c04f32f0d3a66e991a6f22005e1a0
plucky <plucky@echo.engineer>
2021-09-06 22:50:00 +0800

    [feature] fpsHelper

--- app/src/main/java/engineer/echo/study/cmpts/apm/FpsHelper.kt
+++ app/src/main/java/engineer/echo/study/cmpts/apm/FpsHelper.kt
@@ -2,11 +2,13 @@ package engineer.echo.study.cmpts.apm
 
 import android.util.Log
 import android.view.Choreographer
+import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicLong
 
 class FpsHelper : Choreographer.FrameCallback {
 
     private val lastFrameTime = AtomicLong()
+    private val enable = AtomicBoolean(false)
     private var frameThreshold = 30
 
     // 1s 60帧 对应 1帧 16.67ms
@@ -18,31 +20,39 @@ class FpsHelper : Choreographer.FrameCallback {
         }
         // 连续两帧之间的时间差
         val diff = frameTimeNanos - lastFrameTime.get()
-        if (diff >= frameRerNanoTime) {
-            val droppedFrames = diff / frameRerNanoTime
-            if (droppedFrames > frameThreshold) {
-                Log.i(
-                    TAG,
-                    "droppedFrames = " + droppedFrames
-                            + "The application may be doing too much work on its main thread."
-                )
-            }
+        val droppedFrames = if (diff >= frameRerNanoTime) diff / frameRerNanoTime else 0
+        if (DEBUG_MODE.get()) {
+            Log.i(TAG, "doFrame x1 droppedFrames = $droppedFrames")
+        }
+        if (droppedFrames > frameThreshold) {
+            Log.i(
+                TAG,
+                "x2 droppedFrames = " + droppedFrames
+                        + " The application may be doing too much work on its main thread."
+            )
         }
         lastFrameTime.set(frameTimeNanos)
         // 循环处理
-        Choreographer.getInstance().postFrameCallback(this)
+        if (enable.get()) {
+            Choreographer.getInstance().postFrameCallback(this)
+        } else {
+            Choreographer.getInstance().removeFrameCallback(this)
+            lastFrameTime.set(0)
+        }
     }
 
     fun start() {
+        enable.set(true)
         Choreographer.getInstance().postFrameCallback(this)
     }
 
     fun stop() {
-        Choreographer.getInstance().removeFrameCallback(this)
+        enable.set(false)
     }
 
     companion object {
         private const val TAG = "FpsHelper"
+        val DEBUG_MODE = AtomicBoolean(true)
     }
 
 }
--- app/src/main/java/engineer/echo/study/ui/anr/AnrFragment.kt
+++ app/src/main/java/engineer/echo/study/ui/anr/AnrFragment.kt
@@ -9,6 +9,7 @@ import engineer.echo.oneactivity.core.Request
 import engineer.echo.study.R
 import engineer.echo.study.cmpts.BaseFragment
 import engineer.echo.study.cmpts.apm.AnrWatchDog
+import engineer.echo.study.cmpts.apm.FpsHelper
 import engineer.echo.study.databinding.FragmentAnrBinding
 
 class AnrFragment : BaseFragment() {
@@ -16,6 +17,10 @@ class AnrFragment : BaseFragment() {
     private var binding: FragmentAnrBinding? = null
     private val anrWatchDog by lazy {
         AnrWatchDog()
+    }
+
+    private val fpsHelper by lazy {
+        FpsHelper()
     }
 
     override fun getTitle(): Int {
@@ -35,6 +40,7 @@ class AnrFragment : BaseFragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         binding?.tvStop?.performClick()
+        binding?.tvStopFps?.performClick()
         binding = null
     }
 
@@ -45,6 +51,12 @@ class AnrFragment : BaseFragment() {
         }
         binding?.tvStop?.setOnClickListener {
             anrWatchDog.stop()
+        }
+        binding?.tvStartFps?.setOnClickListener {
+            fpsHelper.start()
+        }
+        binding?.tvStopFps?.setOnClickListener {
+            fpsHelper.stop()
         }
     }
 
